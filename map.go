1091f2293ec998ba7f2a108732b5b374c78ee485
Krzysztof Kula <krzychukula@gmail.com>
2014-11-03 10:32:05 +0100

    non existing key

--- map.go
+++ map.go
@@ -6,4 +6,23 @@ func main() {
 	x := make(map[string]int)
 	x["key"] = 10
 	fmt.Println(x)
+
+	y := make(map[int]int)
+	y[1] = 10
+	fmt.Println(y)
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Un"])
+
 }
